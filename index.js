cb2b17b85bc6951c87eb62f1a63ef45f1e238efe
LeoLim27 <arch4614s@gmail.com>
2024-08-27 05:08:47 +0900

    change fetch/then to async/await and add error handling

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
 
-// call needed info by 'click' button. need to add 'Enter' as well.
+// search time measurement
 var startTime = 0;
 var endTime = 0;
 
-search.addEventListener('click', () => {
+// changed fetch/then -> async/await
+search.addEventListener('click', async() => {
     startTime = Date.now();
     console.log(`Start Time is ${startTime}`);
     const APIKey = api;
@@ -22,76 +23,82 @@ search.addEventListener('click', () => {
     // need to pares the longitude and latitude to get weather info
     // first fetch the longitude and latitude of the city by using geo API 
     console.log(`Fetching data for a city: ${city}`);
-    fetch (`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIKey}`)
-        .then(response => {
-            console.log(`Geo API response:`, response);
-            return response.json();
-        })
-        .then(json => {
-            console.log('Geo API data:', json);
-            const lat = json[0].lat;
-            const lon = json[0].lon;
-            // opeenWeather 'current weather data' API call
-            return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}`);
+    try {
+        const geoResponse = await fetch (`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIKey}`)
+        if (!geoResponse.ok) throw new Error('Geo API error')
+        else console.log('Geo API response', geoResponse);
 
-        })
-        .then(response => {
-            console.log('Weather API response:', response);
-            return response.json();
-        })
-        .then(json => {
-            console.log('Weather API data', json);
-            // location not found is not working currently
-            if (json.cod === '404') {
-                container.style.height = '400px';
-                weatherBox.style.display = 'none';
-                weatherDetails.style.display = 'none';
-                error404.style.display = 'block';
-                error404.classList.add('fadeIn');
-                return;
-            }
+        const geoData = await geoResponse.json();
+        console.log('Geo API Data', geoData);
 
-            error404.style.display = 'none';
-            error404.classList.remove('fadeIn');
+        const lat = geoData[0].lat;
+        const lon = geoData[0].lon;
 
-            const image = document.querySelector('.weather-box img');
-            const temperature = document.querySelector('.weather-box .temperature');
-            const description = document.querySelector('.weather-box .description');
-            const humidity = document.querySelector('.weather-details .humidity span');
-            const wind = document.querySelector('.weather-details .wind span');
+        const weatherResponse = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}`);
+        if (!weatherResponse.ok) throw new Error('Weather API error');
+        else console.log('Weather API response:', weatherResponse);
 
-            switch (json.weather[0].main) {
-                case 'Clear':
-                    image.src = 'images/clear.png';
-                    break;
-                case 'Rain':
-                    image.src = 'images/rain.png';
-                    break;
-                case 'Snow':
-                    image.src = 'images/snow.png';
-                    break;
-                case 'Haze':
-                    image.src = 'images/haze.png';
-                    break;
-                case 'Clouds':
-                    image.src = 'images/cloud.png';
-                    break;
-                default:
-                    image.src = '';
-            }
+        const weatherData = await weatherResponse.json();    
+        console.log('Weather API data', weatherData);
 
-            temperature.innerHTML = `${Math.round((parseInt(json.main.temp)-273.15)*10)/10}<span> °C</span>`;
-            description.innerHTML = `${json.weather[0].description}`;
-            humidity.innerHTML = `${json.main.humidity}%`;
-            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+        // when the weather data for corresponding longitude and latitude is not found
+        if (weatherData.cod === '404') {
+            container.style.height = '400px';
+            weatherBox.style.display = 'none';
+            weatherDetails.style.display = 'none';
+            error404.style.display = 'block';
+            error404.classList.add('fadeIn');                
+            return;
+        }
 
-            weatherBox.style.display = '';
-            weatherDetails.style.display = '';
-            weatherBox.classList.add('fadeIn');
-            weatherDetails.classList.add('fadeIn');
-            container.style.height = '590px';
-            endTime = Date.now();
-            console.log(`endTime is ${endTime}`);
-            console.log(`Elapsed Time is ${endTime-startTime}`);
-     });
+        error404.style.display = 'none';
+        error404.classList.remove('fadeIn');
+
+        const image = document.querySelector('.weather-box img');
+        const temperature = document.querySelector('.weather-box .temperature');
+        const description = document.querySelector('.weather-box .description');
+        const humidity = document.querySelector('.weather-details .humidity span');
+        const wind = document.querySelector('.weather-details .wind span');
+        
+        // need to add the case for thunderstorm
+        switch (weatherData.weather[0].main) {
+            case 'Clear':
+                image.src = 'images/clear.png';
+                break;
+            case 'Rain':
+                image.src = 'images/rain.png';
+                break;
+            case 'Snow':
+                image.src = 'images/snow.png';
+                break;
+            case 'Haze':
+                image.src = 'images/haze.png';
+                break;
+            case 'Clouds':
+                image.src = 'images/cloud.png';
+                break;
+            default:
+                image.src = '';
+        }
+        
+        // round up the temperature to one decimal place.
+        temperature.innerHTML = `${Math.round((parseInt(weatherData.main.temp)-273.15)*10)/10}<span> °C</span>`;
+        description.innerHTML = `${weatherData.weather[0].description}`;
+        humidity.innerHTML = `${weatherData.main.humidity}%`;
+        wind.innerHTML = `${parseInt(weatherData.wind.speed)}Km/h`;
+
+        weatherBox.style.display = '';
+        weatherDetails.style.display = '';
+        weatherBox.classList.add('fadeIn');
+        weatherDetails.classList.add('fadeIn');
+        container.style.height = '590px';
+
+        endTime = Date.now();
+        console.log(`endTime is ${endTime}`);
+        console.log(`Elapsed Time is ${endTime-startTime}`);
+
+     } catch (error){
+        console.error(`Error Occured: ${error}`);
+        alert(`error has been occured: ${error.message}`);
+    }
 });
